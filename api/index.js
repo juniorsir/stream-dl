d2dc00c25288769ec7d1b37a58ec8802f4645436
Pradeep kumar Bairwa <sirjunior744@gmail.com>
2025-08-15 06:15:14 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -64,7 +64,7 @@ app.use(express.static(path.join(projectRoot, 'public'))); // Serve static files
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 50, // Limit each IP to 50 requests per window (adjust as needed)
+    max: 200, // Limit each IP to 50 requests per window (adjust as needed)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: { error: 'Too many requests, please try again after 15 minutes.' },
